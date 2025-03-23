a7fe1f2072d8b5a2cf2c3f5c3ab93907142bba5d
Cody <gibbyDEV@protonmail.com>
2025-03-23 11:27:26 -0400

    added home.nix to laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,98 +1,132 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];  # Include hardware scan results.
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # System Settings
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;  # Enable NetworkManager
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Detroit";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User Configuration
   users.users.cody = {
     isNormalUser = true;
     description = "cody";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm" "qemu" "docker" ];
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # Display & Audio Configuration
+  services = {
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+    };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+    # PipeWire audio setup
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+  };
+
+  # Hyprland Window Manager
+  programs.hyprland.enable = true;
+
+  # Virtualization (KVM/QEMU)
+  virtualisation = {
+    libvirtd.enable = true;
+    kvmgt.enable = true;
+  };
+
+  boot.kernelModules = [
+    "kvm" 
+    "kvm-intel"
+    "v4l2loopback"
+  ]; 
+
+  # Fonts Configuration
+  fonts = {
+    fontconfig.enable = true;
+    fontconfig.defaultFonts.monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+    packages = with pkgs; [
+      nerd-fonts.meslo-lg
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.agave
+      font-awesome
+      powerline-fonts
+      powerline-symbols
+    ];
+  };
+
+  # System Packages
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#    wget
+    vim
+    vscode
+    wget
     git
+    gnumake42
+    go
+    cmake
+    gcc
+    gdb
+    networkmanagerapplet
+    pywalfox-native
+    droidcam
+    android-tools
+    davinci-resolve
+    vlc
+    adb-sync
+    libsForQt5.qt5.qtgraphicaleffects
+    linuxPackages.v4l2loopback  # Kernel module for virtual webcam
+    alsa-utils  # Audio support
+    v4l-utils   # Video4Linux tools
+    pipewire
+    qemu
+    libvirt
+    virt-manager
+    bridge-utils  # For networking support
+    dnsmasq       # DHCP for VM networking
+    ebtables      # NAT support for VMs
+
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Specify the SDDM theme configuration
+#  environment.etc."sddm.conf".text = lib.mkForce ''
+#    [Theme]
+#    Current=sugar-candy
+#  '';
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  # Miscellaneous Settings
+  nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # System State Version
+  system.stateVersion = "24.11";
 }
+
