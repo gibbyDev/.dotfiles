4619c4176b024936e3aa7ff8de3ce789166c98de
cody <gibbyDEV@protonmail.com>
2026-02-12 18:23:16 -0500

    added .gitignore

---
+++ hosts/nixos/configuration.nix
@@ -0,0 +1,172 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ]; # Include hardware scan results.
+
+  boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
+  # System Settings
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true; # Enable NetworkManager
+  };
+
+  time.timeZone = "America/Detroit";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  programs.zsh.enable = true;
+
+  # User Configuration
+  users.users.cody = {
+    isNormalUser = true;
+    description = "cody";
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm" "qemu" "docker" ];
+    packages = with pkgs; [ ];
+    shell = pkgs.zsh;
+  };
+
+  # Services Configuration
+  services = {
+    # X server + Display Manager
+# docker.enable = true;
+    xserver = {
+      enable = true;
+      #
+      # displayManager = {
+      #   sddm.enable = true;
+      # };
+      #
+      xkb = {
+        layout = "us,us";
+        variant = ",dvorak";
+        options = "grp:alt_shift_toggle";
+      };
+    };
+
+    # PipeWire audio setup
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+  #
+  virtualisation = {
+    libvirtd.enable = true;        # Enables libvirt daemon and KVM support
+    docker.enable = true;        # Enable this only if you want Docker
+  };
+
+  programs.virt-manager.enable = true;
+
+  # Hyprland Window Manager
+  programs.hyprland.enable = true;
+
+  # Kernel modules
+  boot.kernelModules = [
+    "kvm"
+    "kvm-intel"
+    "v4l2loopback"
+  ];
+
+  # Fonts Configuration
+  fonts = {
+    fontconfig.enable = true;
+    fontconfig.defaultFonts.monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+    packages = with pkgs; [
+      nerd-fonts.meslo-lg
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.agave
+      font-awesome
+      powerline-fonts
+      powerline-symbols
+    ];
+  };
+
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    docker
+    xhost
+    docker-compose
+    gnumake42
+    go
+    cmake
+    gcc
+    gdb
+    networkmanagerapplet
+    pywalfox-native
+    droidcam
+    android-tools
+    davinci-resolve
+    vlc
+    xcolor
+    adb-sync
+    wl-clipboard
+    cliphist
+    libsForQt5.qt5.qtgraphicaleffects
+    linuxPackages.v4l2loopback # Kernel module for virtual webcam
+    alsa-utils # Audio support
+    v4l-utils # Video4Linux tools
+    pipewire
+    qemu
+    libvirt
+    virt-manager
+    bridge-utils # For networking support
+    dnsmasq # DHCP for VM networking
+    ebtables # NAT support for VMs
+    grim
+    slurp
+    grimblast
+    swappy
+    dunst
+    jq
+    moreutils
+    hyprland
+    libnotify
+    coreutils
+    hyprshade
+    unityhub
+    pkg-config
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXi
+    mesa
+    libGL
+    alsa-lib
+  ];
+
+  # Optional: Specify the SDDM theme configuration
+  environment.etc."sddm.conf".text = lib.mkForce ''
+    [Theme]
+    Current=sugar-candy
+  '';
+
+  # Miscellaneous Settings
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # System State Version
+  system.stateVersion = "25.11";
+}
+
---
+++ hosts/nixos/home.nix
@@ -0,0 +1,108 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../../modules
+  ];
+
+  home.username = "cody";
+  home.homeDirectory = "/home/cody";
+  home.stateVersion = "24.11";
+
+  nixpkgs.config.allowUnfree = true;
+
+  ################################
+  # Packages
+  ################################
+  home.packages = with pkgs; [
+    kitty
+    screenkey
+    docker
+    tree
+    ffmpeg
+    unzip
+    gimp
+    ripgrep
+    fzf
+    lsd
+    lazygit
+    opencode
+    neofetch
+    fastfetch
+    tree
+    chafa
+    git
+    nodejs_20
+    # zig_0_12
+    swww
+    tailwindcss
+    templ
+    imagemagick
+    protobuf
+    air
+    pywal
+    bat
+    w3m
+    nwg-displays
+    (writeShellScriptBin "protoc-gen-go" ''
+      export PATH=$HOME/.go/bin:$PATH
+      go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+    '')
+
+    (writeShellScriptBin "protoc-gen-go-grpc" ''
+      export PATH=$HOME/.go/bin:$PATH
+      go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+    '')
+  ];
+
+  ################################
+  # Zsh
+  ################################
+  # programs.zsh.shellAliases = {
+  #   ls = "lsd --color=auto";
+  #   ll = "lsd -lah --color=auto";
+  # };
+  #
+  home.sessionVariables = {
+    SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+  ################################
+  # Dircolors
+  ################################
+  programs.dircolors.enable = true;
+
+  ################################
+  # Powerlevel10k
+  ################################
+  home.file.".p10k.zsh".source = ../../.p10k.zsh;
+
+  ################################
+  # Activation scripts
+  ################################
+  home.activation.copyScripts =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p ~/.local/share/bin
+      rm -rf ~/.local/share/bin/*
+      cp -r ${../../scripts}/* ~/.local/share/bin/
+      chmod +x ~/.local/share/bin/*
+    '';
+
+  home.activation.copyWallpapers =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p ~/Pictures/wallpapers
+      rm -rf ~/Pictures/wallpapers/*
+      cp -r ${../../wallpapers}/* ~/Pictures/wallpapers/
+    '';
+
+  home.file.".config/lsd/colors.yaml".text = ''
+    date: "white"
+    user: "white"
+    group: "white"
+    # ...any other preferences
+  '';
+
+  ################################
+  programs.home-manager.enable = true;
+}
+
